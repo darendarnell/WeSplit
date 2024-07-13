e8292fdb25ed01b7c91c995af097ff4f116f8b58
Daren Darnell <darnelldaren@gmail.com>
2024-07-13 12:36:03 -0400

    Full app done

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var total = 0.0
+    @State private var tip = 0
+    @State private var numPeople = 1
+    
+    private var grandTotal : Double {
+        let tipPercent = Double(tip)/100
+        return total + (total * tipPercent)
+    }
+    
+    private var splitTotal : Double {
+        return grandTotal / Double(numPeople+2)
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Check Total & Tip Percentage") {
+                    TextField("Check total", value: $total, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                    Picker("Tip percentage", selection: $tip) {
+                        ForEach(0..<100){
+                            Text("\($0)%")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("Number of people") {
+                    Picker("Number of people", selection: $numPeople) {
+                        ForEach(2..<11){
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                Section("Grand Total") {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Amount per person") {
+                    Text(splitTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
         }
         .padding()
     }
